2814d81f3e98ab445cc928aec5bc7248e765cbe2
arpi <rozsnyaiarpad@reea.net>
2022-02-28 09:11:45 +0200

    Add comments

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,20 +1,41 @@
 class CompaniesController < ApplicationController
+  # /
   def index
+    # Search for companies from API
     @companies = get_companies_from_api
+
+    # Get saved Companies numbers
     @saved_companies = Company.pluck(:company_number)
   end
 
+  # /favorite_companies
   def favorite_companies
+    # Gather all saved companies
     @companies = Company.all
+
+    # Init saved_companies with empty Array
     @saved_companies = []
   end
 
+  # /save_companies
+  # save selected companies
   def save_companies
+    # If there is any selected company
     if params[:selected_company]
+      # Init reccord array
       records = []
+
+      # get already saved companies, to prevent double records
       saved_companies = Company.pluck(:company_number)
+
+      # get companies from API
       companies = get_companies_from_api
+
+      # iterate through companies results
       companies['items'].each do |company|
+        # add the company detail to records array
+        # if it is a company selected by the user
+        # if it is a new company record
         records << {
           title:            company['title'],
           company_number:   company['company_number'],
@@ -29,24 +50,34 @@ class CompaniesController < ApplicationController
         } if params[:selected_company].include?(company['company_number']) && !saved_companies.include?(company['company_number'])
       end if companies['items']
 
+      # insert all new data to Company table, if there is any
       Company.insert_all(records) if records.compact.length > 0
       message = "#{records.compact.length} companies were saved"
     else
       message = "No company were selected"
     end
+    # redirect to root path
     redirect_to root_path(search_string: params[:search_string]), alert: message
   end
 
   def delete_companies
+    # delete all selected company
     Company.where(company_number: params[:selected_company]).delete_all
+
+    # redirect to favorite company page
     redirect_to favorite_companies_path, alert: "#{params[:selected_company].length} companies were removed"
   end
 
   private
 
   def get_companies_from_api
+    # if there is nothing to search for, why bother
     return nil unless params[:search_string]
+
+    # Init API Service
     companies_house_api = ChApiService.new
+
+    # search for the required string
     companies_house_api.company_search(params[:search_string]) rescue nil
   end
 end
--- app/services/ch_api_service.rb
+++ app/services/ch_api_service.rb
@@ -6,36 +6,44 @@ class ChApiService
   DEFAULT_API_READ_TIMEOUT = 60
   API_MODE = :live
 
-  attr_reader :api_key, :api_url
-
   def initialize()
+    # API mode switch
     api_mode = (API_MODE == :live ? "" : "SANDBOX_")
+
+    # get the proper API key
     @api_key = ENV.fetch("API_#{api_mode}KEY")
+
+    # throw an error if there is no API key
     raise ArgumentError, "API #{api_mode} key is missing" unless @api_key
 
+    # set the proper api_url, from ENV or default
     @api_url = URI(ENV.fetch("API_#{api_mode}URL") || DEFAULT_API_URL)
+
+    # raise an error if the protocol is not https
     raise ArgumentError, "HTTPS connection required" if @api_url.scheme != "https"
 
+    # set connection timeouts
     @open_timeout = (ENV.fetch("API_OPEN_TIMEOUT") || DEFAULT_API_OPEN_TIMEOUT).to_i
     @read_timeout = (ENV.fetch("API_READ_TIMEOUT") || DEFAULT_API_READ_TIMEOUT).to_i
   end
 
+  # close the current connection
   def end_connection
+    # close the connection
     @connection.finish if @connection&.started?
   end
 
-  def company(id)
-    request("company/#{id}", {})
-  end
-
+  # search for company
   def company_search(query, items_per_page: 1000, start_index: 0)
     request("search/companies", { q: query, items_per_page: items_per_page, start_index: start_index }.compact)
   end
 
+  # alphabetic search for company
   def alphabetic_company_search(query, items_per_page: nil, start_index: nil)
     request("alphabetic-search/companies", { q: query, items_per_page: items_per_page, start_index: start_index }.compact)
   end
 
+  # create API connection
   def connection
     @connection ||= Net::HTTP.new(@api_url.host, @api_url.port).tap do |conn|
       conn.use_ssl = true
@@ -44,13 +52,23 @@ class ChApiService
     end
   end
 
+  # send the request
   def request(path, params = {})
+    # create uri from api_url
     uri = URI.join(@api_url, path)
+
+    # encode the params
     uri.query = URI.encode_www_form(params)
+
+    # send the request
     resp = request_resource(uri)
 
     begin
+      # parse request response
       parse_api_response(resp)
+
+      # end the connection
+      end_connection
     rescue StandardError => e
       raise e
     end
@@ -59,24 +77,27 @@ class ChApiService
   private
 
   def request_resource(uri)
+    # build up the request
     req = Net::HTTP::Get.new(uri)
+
+    # set the authentication
     req.basic_auth(@api_key, "")
-    connection.request req
+
+    # send the request
+    connection.request(req)
   end
 
-  def parse_api_response(response)
-    case response.code
+  def parse_api_response(resp)
+    # parse the response according to response status code
+    case resp.code
     when "200"
-      JSON[response.body]
-    when "302" then { 'location': response["location"] }
+      JSON[resp.body]
     when "401"
       raise ArgumentError, "Authentication error"
     when "404"
       raise ArgumentError, "NotFoundError error"
     when "429"
       raise ArgumentError, "API rate limit exceeded"
-    when "502"
-      raise ArgumentError, "Bad API gateway"
     else
       raise ArgumentError, "Unknown API response"
     end
